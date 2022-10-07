3acb25ae0d076f32c185ddb30d03ae79e6e7d277
Alex Demyan <alexdemyangame@gmail.com>
2022-10-07 20:01:24 +0300

    Added burger menu, other improvements

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+// ===SLICK SLIDER===
   $('.slider').slick({
     slidesToShow: 2,
     responsive:[
@@ -19,4 +21,49 @@ $(document).ready(function(){
       }
     ]
   });
+
+//===MENU BURGER===
+  $('.header__burger').click(function(event) {
+    $('.header__burger,.menu').toggleClass('_active');
+    $('body').toggleClass('lock');
+  });
+
+  $('.menu__item').click(function(event) {
+    if ($('.menu').hasClass('_active')) 
+    {
+      $('.header__burger,.menu').removeClass('_active');
+      $('body').removeClass('lock');
+    }
+  });
+
+//=====HEADER - HIDE as scroll
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+// var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+
+//     document.getElementById("navbar").style.top = "0";
+//   } else {
+//     document.getElementById("navbar").style.top = "calc(-50px - (90 - 50) * ((100vw - 375px) / (1140 - 375)) - 2px)";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
+
+
+//=====HEADER - SHADE as scroll
+  window.addEventListener("scroll", () => {
+    let navbar = document.getElementById("navbar");
+    if (window.innerWidth <= 576) {
+      if (window.pageYOffset >= 60 && !navbar.classList.contains("_shaded")) {
+        navbar.classList.add("_shaded");
+      } 
+      if (window.pageYOffset < 60 && navbar.classList.contains("_shaded")) {
+        navbar.classList.remove("_shaded");
+      }
+    }
+    if (window.innerWidth > 576 && navbar.classList.contains("_shaded")) {
+      navbar.classList.remove("_shaded");
+    }
+  })
+
 });
